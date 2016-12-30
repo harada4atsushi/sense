431839cd856c38812f34f0dde82e33a1dfab3483
a.harada <redhornet96@gmail.com>
2016-12-30 13:07:27 +0900

    画面の遷移修正と、ヘッダーを非表示にする

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'static_pages#top'
+  root 'sentences#next'
+  # root 'static_pages#top'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
